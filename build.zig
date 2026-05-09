903d1c4598342e10a7104d3874304b14f4cddd23
dgv <dgv@keemail.me>
2026-05-09 17:12:39 -0300

    rev build imports

--- build.zig
+++ build.zig
@@ -13,23 +13,27 @@ pub fn build(b: *std.Build) !void {
             } },
         },
     );
+    const zmpl_dep = b.dependency("zmpl", .{
+        .target = target,
+        .optimize = optimize,
+        .zmpl_templates_paths = templates_paths,
+    }).module("zmpl");
+    const httpz_dep = b.dependency("httpz", .{
+        .target = target,
+        .optimize = optimize,
+    }).module("httpz");
     const exe = b.addExecutable(.{
         .name = "zig-htmx-tailwind-example",
         .root_module = b.createModule(.{
             .root_source_file = b.path("main.zig"),
             .target = target,
             .optimize = optimize,
+            .imports = &.{
+                .{ .name = "zmpl", .module = zmpl_dep },
+                .{ .name = "httpz", .module = httpz_dep },
+            },
         }),
     });
-    exe.root_module.addImport("zmpl", b.dependency("zmpl", .{
-        .target = target,
-        .optimize = optimize,
-        .zmpl_templates_paths = templates_paths,
-    }).module("zmpl"));
-    exe.root_module.addImport("httpz", b.dependency("httpz", .{
-        .target = target,
-        .optimize = optimize,
-    }).module("httpz"));
     b.installArtifact(exe);
     const run_cmd = b.addRunArtifact(exe);
     if (b.args) |args| {
